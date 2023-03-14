9ef3d3ffeb207597400f6a7f9fce876585eec45d
IrynaPetr <iryna.bieliakova@gmail.com>
2023-03-14 04:22:43 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -15,5 +15,35 @@ const messageEl = document.querySelector('textarea');
 const feedbackFormEl = document.querySelector('form.feedback-form');
 const submitEl = document.querySelector('button');
 const STORAGE_KAY = 'feedback-form-state';
+const formData = {};
 
-feedbackFormEl.addEventListener('submit', pushSubmit);
+feedbackFormEl.addEventListener('submit', onFormSubmit);
+feedbackFormEl.addEventListener('input', throttle(onfeedbackFormEl, 500));
+
+fillForm();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  formData.email = feedbackFormEl.elements.email.value;
+  formData.message = feedbackFormEl.elements.message.value;
+  console.log(formData);
+  feedbackFormEl.reset();
+  localStorage.removeItem(STORAGE_KAY);
+};
+
+function onfeedbackFormEl(e) {
+  formData[e.target.name] =  e.target.value;
+  localStorage.setItem(STORAGE_KAY, JSON.stringify(formData));
+};
+function fillForm() {
+  const savedData = localStorage.getItem(STORAGE_KAY);
+  if (savedData) {
+    const parseSavedData = JSON.parse(savedData);
+    for (const prop in parseSavedData) {
+      if (parseSavedData.hasOwnProperty(prop)) {
+        feedbackFormEl.elements[prop].value = parseSavedData[prop];
+        formData[prop] = parseSavedData[prop];
+      }
+    }
+  }
+};
